3f12f09e7cd17d78aded5bce2f8715771c9627ff
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-11-25 12:08:58 -0800

    created header and card components

---
+++ src/components/SwipeButtons.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../SwipeButtons.css";
+
+
+
+const SwipeButtons = () => {
+  return ( 
+    <div className="swipeButtons">
+
+    </div>
+   );
+}
+ 
+export default SwipeButtons;
