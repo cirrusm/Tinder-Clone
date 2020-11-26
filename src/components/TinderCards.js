54feade5305accef3479b68dcf294df92a283538
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-11-25 18:16:54 -0800

    added buttons and card functionality

--- src/components/TinderCards.js
+++ src/components/TinderCards.js
@@ -1,21 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../TinderCards.css";
 import TinderCard from "react-tinder-card";
-
+import axios from "../axios.js";
 
 const TinderCards = () => {
-  const [people, setPeople] = useState([
-    {
-      name: "Elon Musk",
-      url:
-        "https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg",
-    },
-    {
-      name: "Jeff Bezos",
-      url:
-        "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5bb22ae84bbe6f67d2e82e05%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D560%26cropX2%3D1783%26cropY1%3D231%26cropY2%3D1455",
-    },
-  ]);
+  const [people, setPeople] = useState([]);
+
+  //This is the same as component did mount
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get("/cards");
+
+      setPeople(req.data);
+    }
+    fetchData();
+  }, []);
 
   const swiped = (direction, nameToDelete) => {
     console.log("removing" + nameToDelete);
@@ -39,7 +38,7 @@ const TinderCards = () => {
           >
             <div
               className="card"
-              style={{ backgroundImage: `url(${person.url})` }}
+              style={{ backgroundImage: `url(${person.imgUrl})` }}
             >
               <h3>{person.name}</h3>
             </div>
